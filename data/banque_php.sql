326aead6ab76aa24daedc1d63e5f48ede50f0da2
Adrien Girin <girin.adrien.pro@gmail.com>
2020-10-07 10:04:43 +0200

    update diagram and database

--- data/banque_php.sql
+++ data/banque_php.sql
@@ -1,12 +1,66 @@
 DROP DATABASE IF EXISTS banque_php;
+CREATE DATABASE banque_php CHARACTER SET 'utf8';
+USE banque_php;
 
-CREATE DATABASE banque_php;
+DROP USER IF EXISTS 'BanquePHP'@'Localhost';
+CREATE USER 'BanquePHP'@'Localhost';
+GRANT ALL PRIVILEGES ON banque_php.* To 'BanquePHP'@'Localhost' IDENTIFIED BY 'banque76';
 
-DROP USER IF EXISTS banque_php;
 
+CREATE TABLE User(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  lastname VARCHAR(50) NOT NULL,
+  firstname VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  city VARCHAR(30) NOT NULL,
+  city_code CHAR(5) NOT NULL,
+  adress VARCHAR(50) NOT NULL,
+  sex CHAR(1) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  birth_date DATE NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE=InnoDB;
 
+INSERT INTO User(lastname, firstname, email, city, city_code, adress, sex, password, birth_date)
+VALUES
+("Dupont", "Richard", "r.dupont@gmail.com", "Rouen", "76000", "9 rue du gros horloge", "h", "Riri1962!", "1962-05-21"),
+("Melez", "Claire", "clairemelez@outlook.com", "Lille", "59100", "45 rue du Molinel", "f", "AstraGirl154", "1989-11-14");
 
-CREATE TABLE `banque_php`.`account` ( `id` INT NOT NULL AUTO_INCREMENT , `number_account` VARCHAR(30) NOT NULL , `amount` FLOAT NOT NULL , `user_id_cle_etrangere` INT NOT NULL , `account_type` VARCHAR(30) NOT NULL , PRIMARY KEY (`id`) INDEX (`user_id_cle_etrangere`)) ENGINE = InnoDB;
-CREATE TABLE `banque_php`.`transaction` ( `id` INT NOT NULL AUTO_INCREMENT , `account_id_cle_etrangere` INT NOT NULL , `opération_amount` FLOAT NOT NULL , `date _ransaction` DATE NOT NULL , `name_transaction` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)INDEX (`account_id_cle_etrangere`)) ENGINE = InnoDB;
-CREATE TABLE `banque_php`.`user` ( `id` INT NOT NULL AUTO_INCREMENT , `password_user` VARCHAR(50) NOT NULL , `name_user` VARCHAR(30) NOT NULL , `surname_user` VARCHAR(30) NOT NULL , `date_of_birth` DATE NOT NULL , `mail_address` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE Account(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  amount DECIMAL(11,2) NOT NULL,
+  opening_date TIMESTAMP NOT NULL,
+  account_type VARCHAR(50) NOT NULL,
+  user_id INT UNSIGNED,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES User(id)
+)
+ENGINE=InnoDB;
 
+INSERT INTO Account(amount, opening_date, account_type, user_id)
+VALUES
+(596.23, NOW(), "Compte courant", 1),
+(12345, NOW(), "Livret A", 1),
+(500, NOW(), "PEL", 1),
+(-56.78, NOW(), "Compte courant", 2);
+
+CREATE TABLE Operation(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  operation_type VARCHAR(30) NOT NULL,
+  amount DECIMAL(11,2) NOT NULL,
+  registered TIMESTAMP NOT NULL,
+  label VARCHAR(50) NULL,
+  account_id INT UNSIGNED,
+  PRIMARY KEY (id),
+  FOREIGN KEY (account_id) REFERENCES Account(id)
+)
+ENGINE=InnoDB;
+
+INSERT INTO Operation(operation_type, amount, registered, label, account_id)
+VALUES
+("débit", -15.60, NOW(), "le poulet d'or SARL", 1),
+("crédit", 500, NOW(), NULL, 2),
+("débit", -7.62, NOW(), "carrefour essence", 2),
+("débit", -50, NOW(), "frais de gestion", 2)
+;
